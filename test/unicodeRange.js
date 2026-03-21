ae58ca03f846d5e95942875bf163f66d4c6cdb1c
Andreas Lind <andreas.lind@workday.com>
2026-03-21 15:50:43 +0100

    Drive-by: Add test for unicodeRange generation

--- test/unicodeRange.js
+++ test/unicodeRange.js
@@ -6,4 +6,12 @@ describe('unicode range', function () {
   it('should compress into a range', function () {
     expect(unicodeRange([0x64, 0x20, 0x62, 0x63]), 'to equal', 'U+20,U+62-64');
   });
+
+  it('should handle emojis', function () {
+    expect(
+      unicodeRange(['📋', '🔑', '🫆'].map((c) => c.codePointAt(0))),
+      'to equal',
+      'U+1F4CB,U+1F511,U+1FAC6'
+    );
+  });
 });
